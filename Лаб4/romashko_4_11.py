5a56b8f7422b3224632abf3e2f64783e25e659d8
RomashkoG <145116019+RomashkoG@users.noreply.github.com>
2023-12-04 17:41:33 +0200

    Delete Лаб4 directory

--- Лаб4/romashko_4_11.py
+++
@@ -1,9 +0,0 @@
-l_num = []
-while True:
-    num = int(input())
-    if num == 0:
-        break
-    else:
-     l_num.append(num)
-
-print(sum(l_num))
--- Лаб4/romashko_4_8.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-i = 1
-i1 = 0
-while True:
-    i1 = i**3
-    i += 1
-    if i1 >= n:
-        break
-    print(i1, end=" ")
